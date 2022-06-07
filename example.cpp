f8242ab1579db109bba9e5c73882d37f10ca0b87
Pranjal Patel <pranjalpatel9838@gmail.com>
2022-06-07 18:28:33 +0530

    Example Code

---
+++ example.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+#include "include/FileSystemWatcher.h"
+
+void print(bool &running_);
+
+int main() {
+  const char* path = "./file_to_be_monitored.txt";
+  
+  FileWatcher::Watcher dog(path);
+
+  std::cout << "watching...\n";
+  dog.watch(FileWatcher::Status::Modified, print);
+}
+
+void print(bool &running_) {
+  // you can set `running_` to false if you don't wanna monitor the file anymore
+
+  std::cout << "File Changed\n";
+}
